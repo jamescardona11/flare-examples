2ff354b282c87b580870e96a595197aa191ce6dd
jamescardona11 <jjcardonao@gmail.com>
2020-09-26 23:12:37 -0500

    Init flare examples

---
+++ android/app/src/main/kotlin/co/kodevincere/flare_examples/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.kodevincere.flare_examples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flare_examples/bottommenu/bottom_menu_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(child: Text('FLARE')),
+        ),
+        body: _NavigatorIntern(),
+      ),
+    );
+  }
+}
+
+class _NavigatorIntern extends StatelessWidget {
+  final Map<String, Widget> screens = {
+    'Bottom Menu': BottomMenuView(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: ListView(
+        children: convertMapToList(context, screens),
+      ),
+    );
+  }
+}
+
+List<Widget> convertMapToList(BuildContext context, screens) {
+  List<Widget> listItems = [];
+  screens.forEach(
+    (key, value) => listItems.add(
+      Card(
+        child: ListTile(
+          title: Text(
+            key,
+            style: TextStyle(),
+          ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => value));
+          },
+        ),
+      ),
+    ),
+  );
+
+  return listItems;
+}
